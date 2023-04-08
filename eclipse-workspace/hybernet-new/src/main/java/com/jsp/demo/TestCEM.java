da8cde30f4a39c643453b5f7dddf2fec619d41dd
Deep_Js <sarma.d3e@gmail.com>
2023-04-08 13:23:25 +0530

    test create entity manager

--- eclipse-workspace/hybernet-new/src/main/java/com/jsp/demo/TestCEM.java
+++ eclipse-workspace/hybernet-new/src/main/java/com/jsp/demo/TestCEM.java
@@ -5,13 +5,13 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
-public class TestcEM {
+public class TestCEM {
 
 	public static void main(String[] args) {
 		Person person = new Person();
-		person.setId(1);
-		person.setName("Raju");
-		person.setAge(32);
+		person.setId(3);
+		person.setName("Rajnandani");
+		person.setAge(22);
 
 		EntityManagerFactory entityManagerFactory=Persistence.createEntityManagerFactory("god");
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
